8e822e401a1f2f264c23e3fb7f3499edae277ff1
ansh <anshgravity@gmail.com>
2020-07-02 17:59:59 +0530

    faster_rcnn_mobilenet_v2_feature_extractor added

--- models/faster_rcnn_mobilenet_v2_feature_extractor.py
+++ models/faster_rcnn_mobilenet_v2_feature_extractor.py
@@ -110,7 +110,6 @@ class FasterRCNNMobilenetV2FeatureExtractor(
           slim.arg_scope(
               [mobilenet.depth_multiplier], min_depth=self._min_depth):
         with (slim.arg_scope(self._conv_hyperparams_fn())
-              if self._override_base_feature_extractor_hyperparams else
               context_manager.IdentityContextManager()):
           _, activations = mobilenet_v2.mobilenet_base(
               ops.pad_to_multiple(preprocessed_inputs, self._pad_to_multiple),
